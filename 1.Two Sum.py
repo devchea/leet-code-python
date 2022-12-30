0d920991894536dced3cd768ad9524c2a19956e0
devchea <richardchea@Richards-MacBook-Pro.local>
2022-12-29 21:54:26 -0800

    update

--- 1.Two Sum.py
+++ 1.Two Sum.py
@@ -1,12 +1,9 @@
 def two_sum(nums=[2, 7, 11, 15], target=9):
-    #create dictionary
+    # create dictionary
     this_dict = {}
-
-
 
     print(this_dict)
     print(nums)
-    print(target)
 
 
 two_sum()
